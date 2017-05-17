82a55c772b12cf23ae5003a1864354e62cd55e94
Devin Dixon <devinisafoal@gmail.com>
2017-05-16 21:06:00 -0600

    add addFavorite action

--- app/Actions/index.js
+++ app/Actions/index.js
@@ -1,24 +1,23 @@
-export const createAccout = (name, password, email) => {
+export const createAccount = (name, password, email, id) => {
   return {
     type: 'CREATE_ACCOUNT',
     name,
     password,
-    email
+    email,
+    id
   }
 }
 
-export const signIn = (email, password) => {
+export const signIn = (user) => {
   return {
     type: 'SIGN_IN',
-    email,
-    password
+    user
   }
 }
 
 export const signOut = () => {
   return {
     type: 'SIGN_OUT'
-
   }
 }
 
@@ -29,9 +28,10 @@ export const receiveMovies = (movies) => {
   }
 }
 
-export const showFavorites= () => {
+export const showFavorites= (favorites) => {
   return {
     type: 'SHOW_FAVORITES'
+    favorites
   }
 }
 
@@ -39,14 +39,24 @@ export const fetchMovies = () => {
   return dispatch => {
     fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=cbe22e12e2a525d944ad1729b43212d3&language=en-US&page=1')
     .then((response) => {
-      return response.json()
+      return response.json();
     })
     .then((movies) => {
       console.log(movies.results);
-      dispatch(receiveMovies(movies))
+      dispatch(receiveMovies(movies));
     })
     .catch(() => {
-      console.log('fetching error')
+      console.log('fetching error');
+    });
+  }
+}
+
+export const addFavorite = (info) => {
+  return dispatch => {
+    fetch('api/users/favorites/new', {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({ info })
     });
   }
 }
