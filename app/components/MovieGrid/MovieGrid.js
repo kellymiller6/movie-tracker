8b1aa93421e49614000397e7b15c1dac2fba517a
Kelly Miller <kellyannemiller@gmail.com>
2017-05-18 10:28:06 -0600

    make movie-grid our app

--- app/components/MovieGrid/MovieGrid.js
+++ app/components/MovieGrid/MovieGrid.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import Movie from '../Movie/Movie'
+import MovieGridContainer from '../MovieGrid/MovieGridContainer';
+import CreateUserContainer from '../CreateUser/CreateUserContainer';
+import UserLoginContainer from '../Login/UserLoginContainer';
 
 export default class MovieGrid extends Component {
   constructor() {
@@ -19,8 +22,13 @@ export default class MovieGrid extends Component {
     })
 
     return (
-      <div className='header'>
-        <h1>Movie Watcher</h1>
+      <div className='app'>
+        <UserLoginContainer />
+        <CreateUserContainer />
+
+        <div className='header'>
+          <h1>Movie Watcher</h1>
+        </div>
         <div className='movie-grid'>
           {movies}
         </div>
--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
-import App from './components/App/App';
+import MovieGridContainer from './components/MovieGrid/MovieGridContainer';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
@@ -14,7 +14,7 @@ const store = createStore(rootReducer, devTools, applyMiddleware(thunk));
 
 render(
   <Provider store={store} >
-    <App />
+    <MovieGridContainer />
   </Provider>,
   document.getElementById('main')
 );
