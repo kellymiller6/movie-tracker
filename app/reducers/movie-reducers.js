bda03b5e9d52ee127d8eb34af6d70a9669ebbc48
Devin Dixon <devinisafoal@gmail.com>
2017-05-19 14:45:03 -0600

    update favorite reducer

--- app/reducers/movie-reducers.js
+++ app/reducers/movie-reducers.js
@@ -2,15 +2,6 @@ const movieReducer = (state=[], action) => {
   switch(action.type){
     case 'RECEIVE_MOVIES':
       return [...state, ...action.movies.results];
-    case 'SHOW_FAVORITES':
-      return [...action.favorites];
-    case 'TOGGLE_FAVORITES':
-      return state.map(movie => {
-        if(movie.id !== action.id) {
-          return movie;
-        }
-        return Object.assign({}, todo, {favorited: !movie.favorited});
-      })
     default:
       return state;
   }
