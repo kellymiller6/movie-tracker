fbeb56da3aa0757ef29e57fe66c5f98ce58dc5a7
Devin Dixon <devinisafoal@gmail.com>
2017-05-21 08:56:14 -0600

    finally done with favoriting logic

--- app/components/Favorites/Favorites.js
+++ app/components/Favorites/Favorites.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Favorites = ({user, deleteFavorite, fetchFavorites, favorites}) => {
 
-  const faves = () => favorites.map((movie, index) => {
-      return <Movie key={index}
-                    movieData={movie}
-                    user={user}
-                    addFavorite={''}
-                    deleteFavorite={deleteFavorite}
-                    fetchFavorites={fetchFavorites}
-                    favorites={favorites}
-                  />
+  const faves = () => favorites.map(movie => {
+      return (
+        <div className='movie-card'>
+          <div className='container'>
+            <img className='movie-img'
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            />
+          </div>
+          <button className='mark-favorite favorited'
+                  onClick={() => { deleteFavorite(user, movie.movie_id)
+                             return fetchFavorites(user) } }>
+                  Favorite
+                </button>
+          <h3 className='movie-title'>{movie.title}</h3>
+          <p className='movie-overview'>Release Date: {movie.release_date}</p>
+          <p className='movie-overview'>{movie.overview}</p>
+          <p className='movie-overview'>Vote Average: {movie.vote_average}</p>
+        </div>
+      )
   });
 
   return (
