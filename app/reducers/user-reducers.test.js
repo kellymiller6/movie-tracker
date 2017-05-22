80cc0406e612536a14f344db9992ab5ae2e3e1fd
Justyna Field <justyna.field@gmail.com>
2017-05-21 19:37:50 -0600

    align cards on favorites page

--- app/reducers/user-reducers.test.js
+++ app/reducers/user-reducers.test.js
@@ -1,44 +1,50 @@
 import userReducer from './user-reducers';
 
 describe('userReducer', () => {
-  it('should return a default state', () => {
+
+
+  it('should return default state', () => {
     const mockUser = {
       name: '',
       email: '',
       password: '',
       id: ''
-    };
+    }
 
     expect(userReducer(undefined, {})).toEqual(mockUser);
-  });
+  })
 
   it('should create an account', () => {
-    const mockUser = {
-      name: 'Andrew',
-      email: 'andrew@gmail.com',
-      password: 'password',
-    };
 
-    expect(userReducer(mockUser, 'CREATE_ACCOUNT')).toEqual(mockUser);
-  });
+  const mockUser = {
+    name: 'Andrew',
+    email: 'andrew@gmail.com',
+    password: 'password',
+  }
 
-  it('should sign the user in', () => {
+    expect(userReducer(mockUser, 'CREATE_ACCOUNT')).toEqual(mockUser)
+  })
+
+  it('should sign in', () => {
     const mockUser = {
       email: 'andrew@gmail.com',
       password: 'password'
-    };
+    }
 
     expect(userReducer(mockUser, 'SIGN_IN')).toEqual(mockUser);
-  });
+  })
 
-  it('should sign the user out', () => {
+  it('should be able to sign out', () => {
+
     const mockUser = {
       name: '',
       email: '',
       password: '',
       id: ''
-    };
+    }
 
-    expect(userReducer(undefined, 'SIGN_OUT')).toEqual(mockUser);
-  });
-});
+    expect(userReducer(undefined, 'SIGN_OUT')).toEqual(mockUser)
+
+  })
+
+})
