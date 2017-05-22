de917bc4f604cd893f2de312200ffc9cb36ca6c3
Devin Dixon <devinisafoal@gmail.com>
2017-05-21 23:23:30 -0600

    add test for movie

--- app/components/Movie/Movie.test.js
+++ app/components/Movie/Movie.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import { browserHistory } from 'react-router';
+
 import Movie from './Movie';
 import { addFavorite, fetchFavorites, deleteFavorite} from '../../reducers/index.js'
 
@@ -12,14 +14,34 @@ describe('Movie', () => {
     average: 5.6
   }
 
-  const favorites = {}
+  const favorites = {};
 
   it('should render a movie card', () => {
-    let wrapper = shallow(<Movie movieData={movie} user='' addFavorite={addFavorite} deleteFavorite={deleteFavorite} fetchFavorites={fetchFavorites} favorites={favorites} />)
+    const wrapper = shallow(<Movie movieData={movie}
+                                   user=''
+                                   addFavorite={addFavorite}
+                                   deleteFavorite={deleteFavorite}
+                                   fetchFavorites={fetchFavorites}
+                                   favorites={favorites} />);
+
     expect(wrapper.find('.movie-card').length).toBe(1)
-
     expect(wrapper.find('.movie-title').text()).toBe('The Boss Baby')
-  })
+  });
 
+  it('should redirect to login if user is not signed in', () => {
+    spyOn(browserHistory, 'replace');
+    const wrapper = mount(<Movie movieData={movie}
+                                 user=''
+                                 addFavorite={addFavorite}
+                                 deleteFavorite={deleteFavorite}
+                                 fetchFavorites={fetchFavorites}
+                                 favorites={favorites}
+                                 history={browserHistory}/>);
 
-})
+    const favoriteBtn = wrapper.find('button');
+
+    favoriteBtn.simulate('click');
+
+    expect(browserHistory.replace).toHaveBeenCalledWith('/login');
+  });
+});
