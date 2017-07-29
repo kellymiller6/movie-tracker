d757d4ae8b25479a743c1d34dd14f1025ee7bd2f
Kelly Miller <kellyannemiller@gmail.com>
2017-07-29 12:11:26 -0600

    add webpack prod file

--- app/index.js
+++ app/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
-
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
@@ -11,14 +10,9 @@ import rootReducer from './reducers';
 import App from './components/App/App';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
 const history = createHistory();
-
 const middleware = routerMiddleware(history);
-
 const store = createStore(rootReducer, devTools, applyMiddleware(thunk));
-
-
 
 render(
   <Provider store={store} >
